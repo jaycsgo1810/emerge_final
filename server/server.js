82ffa29998a4ffec69834bd2864d45e6c3399330
Jayanth Reddy Sripathi <160444498+jaycsgo1810@users.noreply.github.com>
2024-04-21 18:11:36 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
